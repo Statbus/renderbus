419821e94a7387e422b7b9b417b47c5187f5694d
Nick Farley <nick@nfreader.net>
2023-07-12 13:49:05 -0400

    Write revision data to json for the map viewer

--- getmaps.py
+++ getmaps.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
 import os, json
-from datetime import datetime
 import argparse
+import subprocess
 
 parser = argparse.ArgumentParser(description='Smush your codebase json mapfiles into one')
 parser.add_argument('--codebase', metavar='N', type=str, nargs='?',
@@ -11,9 +11,27 @@ parser.add_argument('--out_dir', metavar='N', type=str, nargs='?',
                     help='Path to output for the merged json')
 args = parser.parse_args()
 
+
+def get_git_revision_hash() -> str:
+    return subprocess.check_output(['git', 'rev-parse', 'HEAD'], cwd=codebase).decode('ascii').strip()
+
+def get_git_revision_short_hash() -> str:
+    return subprocess.check_output(['git', 'rev-parse', '--short', 'HEAD'], cwd=codebase).decode('ascii').strip()
+
+def get_git_revision_date() -> str:
+    return subprocess.check_output(['git', 'log', '-n1', '--pretty=%ci', 'HEAD'], cwd=codebase).decode('ascii').strip()
+
 codebase = args.codebase
 out_dir = args.out_dir
 maps_dir = f"{codebase}/_maps"
+
+revision = {}
+revision['hash'] = get_git_revision_hash()
+revision['shorthash'] = get_git_revision_short_hash()
+revision['date'] = get_git_revision_date()
+
+with open(f"{out_dir}/revision.json", 'w') as revisionjson:
+    json.dump(revision, revisionjson)
 
 maps = []
 maplist = []
